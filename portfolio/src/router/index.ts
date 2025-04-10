bcebd9f145b4153d1f47509d639194bae7561821
Sindhu R <blu.fuzzies@gmail.com>
2025-04-10 17:16:20 -0400

    Fixing routing issue

--- portfolio/src/router/index.ts
+++ portfolio/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,7 +18,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
-    { path: '*', component: HomeView },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'Not Found',
+      component: NotFoundView,
+    },
   ],
 })
 
--- portfolio/src/views/NotFoundView.vue
+++ portfolio/src/views/NotFoundView.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
-
 <template>
-  <p>Page Not Found</p>
+  <div class="not-found">
+    <h1>Not Found</h1>
+  </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .not-found {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
